a5443f4a410115f06bbff3a445f04a26511f0aa9
Nguyễn Văn Huy <huynv@overate-vntech.com>
2024-01-19 20:49:55 +0700

    Create the header section

--- client/.eslintrc.cjs
+++ client/.eslintrc.cjs
@@ -1,6 +1,11 @@
 module.exports = {
   root: true,
-  env: { browser: true, es2020: true },
+  env: { 
+    browser: true, 
+    es2020: true ,
+    es60: true,
+    node: true
+  },
   extends: [
     'eslint:recommended',
     'plugin:react/recommended',
