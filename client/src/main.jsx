b9708bfea8a79ae9c3ddce3c2083b9ad5c635c42
Nguyễn Văn Huy <huynv@overate-vntech.com>
2024-01-23 22:11:30 +0700

    Make the dashboard private

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -5,7 +5,6 @@ import './index.css'
 import { store,persistor } from './redux/store.js'
 import { Provider } from 'react-redux'
 import { PersistGate } from 'redux-persist/integration/react'
-import { Spinner } from 'flowbite-react'
 import ThemeProvider from './components/ThemeProvider.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
