a0e9939996262bf7287c6b7fe82680958cef717e
Nguyễn Văn Huy <huynv@overate-vntech.com>
2024-01-25 11:57:10 +0700

    Add signout functionality

--- client/src/components/DashSidebar.jsx
+++ client/src/components/DashSidebar.jsx
@@ -1,39 +1,63 @@
-import { useEffect, useState } from 'react'
-import { useLocation,Link } from 'react-router-dom'
-import { Sidebar } from 'flowbite-react'
-import { HiUser, HiArrowSmRight } from 'react-icons/hi'
+import { useEffect, useState } from "react";
+import { useLocation, Link } from "react-router-dom";
+import { Sidebar } from "flowbite-react";
+import { HiUser, HiArrowSmRight } from "react-icons/hi";
+import { signoutSuccess } from "../redux/user/userSlice";
+import { useDispatch } from "react-redux";
 
 export default function DashSidebar() {
-    const location = useLocation();
-    const [tab, setTab] = useState('');
-    
-    useEffect(() => {
-        const urlParams = new URLSearchParams(location.search);
-        const tabFromUrl = urlParams.get('tab');
-        if(tabFromUrl) {
-        setTab(tabFromUrl);
-        }
-    },[location.search])
+  const location = useLocation();
+  const [tab, setTab] = useState("");
+  const dispatch = useDispatch();
+
+  const handleSignout = async () => {
+    try {
+      const res = await fetch("api/user/signout", {
+        method: "POST",
+      });
+      const data = await res.json();
+      if (!res.ok) {
+        console.log(data.message);
+      } else {
+        dispatch(signoutSuccess());
+      }
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
+
+  useEffect(() => {
+    const urlParams = new URLSearchParams(location.search);
+    const tabFromUrl = urlParams.get("tab");
+    if (tabFromUrl) {
+      setTab(tabFromUrl);
+    }
+  }, [location.search]);
+
   return (
-    <Sidebar className='w-full md:w-56'>
-        <Sidebar.Items>
-            <Sidebar.ItemGroup>
-                <Link to='/dashboard?tab=profile'>
-                    <Sidebar.Item 
-                        active={tab === 'profile'} 
-                        icon={HiUser} 
-                        label={"User"} 
-                        labelColor='dark'
-                        as='div'
-                    >
-                        Profile
-                    </Sidebar.Item>
-                </Link>
-                <Sidebar.Item icon={HiArrowSmRight} className='cursor-pointer'>
-                    Sign Out
-                </Sidebar.Item>
-            </Sidebar.ItemGroup>
-        </Sidebar.Items>
+    <Sidebar className="w-full md:w-56">
+      <Sidebar.Items>
+        <Sidebar.ItemGroup>
+          <Link to="/dashboard?tab=profile">
+            <Sidebar.Item
+              active={tab === "profile"}
+              icon={HiUser}
+              label={"User"}
+              labelColor="dark"
+              as="div"
+            >
+              Profile
+            </Sidebar.Item>
+          </Link>
+          <Sidebar.Item
+            icon={HiArrowSmRight}
+            className="cursor-pointer"
+            onClick={handleSignout}
+          >
+            Sign Out
+          </Sidebar.Item>
+        </Sidebar.ItemGroup>
+      </Sidebar.Items>
     </Sidebar>
-  )
+  );
 }
