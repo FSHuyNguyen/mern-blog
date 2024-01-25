72dd4dfbdc224ac761972bdaf44650ee2bc0a4fc
Nguyễn Văn Huy <huynv@overate-vntech.com>
2024-01-25 10:41:09 +0700

    complete profile page functionality

--- client/src/components/DashProfile.jsx
+++ client/src/components/DashProfile.jsx
@@ -1,18 +1,24 @@
 import React, { useState,useRef, useEffect } from 'react'
-import { useSelector } from 'react-redux'
 import { TextInput,Button, Alert } from 'flowbite-react'
 import { getDownloadURL, uploadBytesResumable,ref } from 'firebase/storage'
 import { storage } from '../utils/firebase'
-import { CircularProgressbar } from 'react-circular-progressbar';
-import 'react-circular-progressbar/dist/styles.css';
+import { CircularProgressbar } from 'react-circular-progressbar'
+import 'react-circular-progressbar/dist/styles.css'
+import { updateStart, updateSuccess, updateFailure } from '../redux/user/userSlice'
+import { useDispatch,useSelector } from 'react-redux'
 
 export default function DashProfile() {
     const { currentUser } = useSelector(state => state.user);
     const [imageFile, setImageFile] = useState(null);
     const [imageFileUrl, setImageFileUrl] = useState(null);
     const [imageFileUploadProgress, setImageFileUploadProgress] = useState(null);
+    const [imageFileUploadLoading,setImageFileUploadLoading] = useState(false);
     const [imageFileUploadError, setImageFileUploadError] = useState(null);
+    const [updateUserSuccess,setUpdateUserSuccess] = useState(null);
+    const [updateUserError,setUpdateUserError] = useState(null);
+    const [formData,setFormData] = useState({});
     const filePickerRef = useRef();
+    const dispatch = useDispatch();
 
     const handleImageChange = (e) => {
         const file = e.target.files[0];
@@ -39,6 +45,7 @@ export default function DashProfile() {
         //       }
         //     }
         //   }
+        setImageFileUploadLoading(true);
         setImageFileUploadError(null);
         const fileName = new Date().getTime() + imageFile.name;
         const storageRef = ref(storage,fileName);
@@ -55,18 +62,62 @@ export default function DashProfile() {
                 setImageFileUploadProgress(null);
                 setImageFileUrl(null);
                 setImageFile(null);
+                setImageFileUploadLoading(false);
             },
             () => {
                 getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
                     setImageFileUrl(downloadURL);
+                    setFormData({...formData, profilePicture: downloadURL});
+                    setImageFileUploadLoading(false);
                 });
             }
         );
     }
+
+    const handleChangeInput = (e) => {
+        setFormData({...formData, [e.target.id]: e.target.value});
+    }
+
+    const handleSubmitUpdateUser = async (e) => {
+        e.preventDefault();
+        setUpdateUserSuccess(null);
+        setUpdateUserError(null);
+        if(Object.keys(formData).length === 0) {
+            setUpdateUserError('No changes made');
+            return;
+        }
+        if(imageFileUploadLoading) {
+            setUpdateUserError('Please wait for image to upload ');
+            return;
+        }
+
+        try {
+            dispatch(updateStart());
+            const res = await fetch(`api/user/update/${currentUser._id}`,{
+                method: 'PUT',
+                headers: { 
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(formData),
+            })
+            const data = await res.json();
+            if(!res.ok) {
+                dispatch(updateFailure(data.message));
+                setUpdateUserError(data.message);
+            } else {
+                dispatch(updateSuccess(data));
+                setUpdateUserSuccess("User's profile updated successfully");
+            }
+        } catch (error) {
+            dispatch(updateFailure(error.message));
+            setUpdateUserError(error.message);
+        }
+    }
+
   return (
     <div className='max-w-lg mx-auto p-3 w-full'>
       <h1 className='my-7 text-center font-semibold text-3xl'>Profile</h1>
-      <form className='flex flex-col gap-4'>
+      <form onSubmit={handleSubmitUpdateUser} className='flex flex-col gap-4'>
         <input type="file" accept='image/*' onChange={handleImageChange} ref={filePickerRef} hidden/>
         <div className="relative w-32 h-32 self-center cursor-pointer shadow-md rounded-full overflow-hidden" 
              onClick={() => filePickerRef.current.click()}>
@@ -98,19 +149,22 @@ export default function DashProfile() {
             />
         </div>
         {imageFileUploadError && <Alert color='failure'>{imageFileUploadError}</Alert>}
-        <TextInput 
+        <TextInput
+            onChange={handleChangeInput} 
             type='text' 
             id='username' 
             placeholder='username' 
             defaultValue={currentUser.username}
             />
-        <TextInput 
+        <TextInput
+            onChange={handleChangeInput} 
             type='email' 
             id='email' 
             placeholder='email' 
             defaultValue={currentUser.email}
             />
-        <TextInput 
+        <TextInput
+            onChange={handleChangeInput} 
             type='password' 
             id='password' 
             placeholder='password' 
@@ -127,6 +181,16 @@ export default function DashProfile() {
         <span className='cursor-pointer'>Delete Account</span>
         <span className='cursor-pointer'>Sign Out</span>
       </div>
+      {updateUserSuccess && (
+        <Alert color='success' className='mt-5'>
+            {updateUserSuccess}
+        </Alert>
+      )}
+      {updateUserError && (
+        <Alert color='failure' className='mt-5'>
+            {updateUserError}
+        </Alert>
+      )}
     </div>
   )
 }
