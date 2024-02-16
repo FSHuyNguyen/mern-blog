7987f4f773353e276f59f125495871d2335107bd
Nguyễn Văn Huy <huynv@overate-vntech.com>
2024-02-16 15:37:39 +0700

    complete about and projects pages

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -1,5 +1,21 @@
 export default function About() {
   return (
-    <div>About</div>
-  )
+    <div className="min-h-screen flex items-center justify-center">
+      <div className="max-w-2xl mx-auto p-3 text-center">
+        <div className="">
+          <h1 className="text-3xl font font-semibold text-center my-7">
+            About FS's Blog
+          </h1>
+          <div className="text-md text-gray-500 flex flex-col gap-6">
+            <p>
+              FS' Blog is a blog that I created to share my thoughts and ideas
+              with the world. I am a software engineer and I love to write about
+              my experiences and things that I have learned. I hope you enjoy
+              reading my blog
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
